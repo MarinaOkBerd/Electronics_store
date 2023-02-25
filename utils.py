8c224948dc14bb51ed765e6127e416ed66a85ea7
Marina Berdnikova <berdni4ka@rambler.ru>
2023-02-25 23:57:06 +0300

    Add file utils.py

--- utils.py
+++ utils.py
@@ -9,7 +9,6 @@ class Item:
         Item.all.append(self)
 
     def calculate_total_price(self):
-
         return self.price * self.count
 
     def apply_discount(self):
@@ -22,13 +21,4 @@ item2 = Item("Ноутбук", 20000, 5)
 print(item1.calculate_total_price())
 print(item2.calculate_total_price())
 
-
-
 Item.pay_rate = 0.8  # устанавливаем новый уровень цен
-item1.apply_discount()
-print(item1.price)
-print(item2.price)
-
-
-
-print(Item.all)
